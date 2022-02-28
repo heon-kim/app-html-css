310a46e819a2a0be5e65052ac0a45e86c6fe0e83
heon-kim <vndtka123@gmail.com>
2022-03-01 04:05:49 +0900

    Finish

---
+++ script.js
@@ -0,0 +1,13 @@
+const clock = document.querySelector("span.clock");
+const header = document.querySelector("i.fa-chevron-up");
+const contents = document.querySelector("div.friends-screen__channel-contents");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
